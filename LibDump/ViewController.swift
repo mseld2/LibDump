38c4bf420b170b5533c736da22678c24bcdca255
Mel <csi.agent32@gmail.com>
2019-10-09 05:20:04 -0500

    Initial Commit

---
+++ LibDump/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LibDump
+//
+//  Created by Melissa Weiss on 10/9/19.
+//  Copyright © 2019 All Tyme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LibDumpTests/LibDumpTests.swift
@@ -0,0 +1,34 @@
+//
+//  LibDumpTests.swift
+//  LibDumpTests
+//
+//  Created by Melissa Weiss on 10/9/19.
+//  Copyright © 2019 All Tyme. All rights reserved.
+//
+
+import XCTest
+@testable import LibDump
+
+class LibDumpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
